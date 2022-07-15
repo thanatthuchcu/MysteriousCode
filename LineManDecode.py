51ecc86606e8c808b888e6c767ed65cec1f0f5a9
thanatthuchcu <104338444+thanatthuchcu@users.noreply.github.com>
2022-07-15 14:22:17 +0700

    LineManDecode.py README.md
    Initial Commit

---
+++ LineManDecode.py
@@ -0,0 +1,26 @@
+import base64
+
+
+
+def LineManDecoder(CODE: str)-> str:
+    # Decode from Base64
+    DecB64 = base64.b64decode(CODE) # The result of DecB64 is >> b'iangnoW:NAM:ENIL:ta:su:nioJ' this is byte form<< 
+    
+    # Reform Byte form to STR form
+    STRform = DecB64.decode("utf-8") # The result of STRform is >> 'iangnoW:NAM:ENIL:ta:su:nioJ'
+    
+    # Reverse text
+    Result = STRform[::-1]
+
+    print(
+        f"The Result is : {Result}"
+    )
+    return Result;
+
+if __name__ == "__main__":
+    print("================================= Start ====================================")
+    CODE = "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K";
+    LineManDecoder(CODE);
+
+    print("\nThank for consider\nMy full name: Thanatthuch Kamseng\nRole: Machine Learning Engineer\n*ContractMe*\nLine: thanatthuch38\nCall: 085-115-4196\nEmail: thanatthuchkamseng@gmail.com")
+    print("================================= ENDL ====================================")
